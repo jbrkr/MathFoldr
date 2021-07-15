ba1277d814d43a4e33dfa417119c0cb9658520e6
Joe Brucker <joe.r.brucker@gmail.com>
2021-07-14 22:08:53 -0400

    fe

---
+++ frontend/src/common/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>MathFoldr</h1>
+            <div className="Header-Subtitle">Topos Institute, California</div>
+    </div>
+  )
+}
+
+export default Header
