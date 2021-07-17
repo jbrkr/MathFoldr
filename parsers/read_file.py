3165b1044a1eff25bdb7d2845b9ea22a151d6b20
Joe Brucker <joe.r.brucker@gmail.com>
2021-07-17 08:16:21 -0400

    preprocessing, general

---
+++ parsers/read_file.py
@@ -0,0 +1,15 @@
+import time
+import json
+
+
+def read_json_file(path):
+    print("Processing: " + path)
+    t1 = time.time()
+    with open(path) as f:
+        data = json.load(f)
+    t = time.time() - t1
+    print(t)
+    print() 
+    return data
+
+    
